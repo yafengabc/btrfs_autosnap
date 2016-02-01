0228877693f0c72af48ceadc470467651562df62
yafengabc <yafengabc@gmail.com>
2016-02-01 12:03:13 +0000

    add check rootfs

--- btrfs_autosnap/btrfs_autosnap.py
+++ btrfs_autosnap/btrfs_autosnap.py
@@ -30,6 +30,15 @@ def get_grub_header():
                 break
     fi.close()
 
+#check the subvol is rootfs?
+def check_rootfs(root):
+    pip=os.popen("mount |grep "+root)
+    m_str=pip.readlines()
+    if str(m_str).find(' / ')!=-1:
+        return True
+    else:
+        return False
+
 #Get the command line argvs
 def get_cmd():
     device=root=''
@@ -48,15 +57,17 @@ def manage_snap(device,root):
     while len(snap_vols)>5:
         os.system('btrfs subvol delete /mnt/'+snap_vols.pop())
     #add some boot entry for snapshot
-    get_grub_header()
-    for i in snap_vols:
-        bootstring=autostring
-        tmp=def_entry.replace('Arch Linux','Arch Linux '+root)
-        bootstring+=tmp.replace(root,i)
-        grub_header+=bootstring
-    fi=open('/boot/grub/grub.cfg','w')
-    fi.write(grub_header)
-    fi.close()
+    if check_rootfs(root):
+        print("make grub enty")
+        get_grub_header()
+        for i in snap_vols:
+            bootstring=autostring
+            tmp=def_entry.replace('Arch Linux','Arch Linux '+root)
+            bootstring+=tmp.replace(root,i)
+            grub_header+=bootstring
+        fi=open('/boot/grub/grub.cfg','w')
+        fi.write(grub_header)
+        fi.close()
 
 #make a snapshot 
 def make_snap(device,snap_s):
@@ -71,3 +82,4 @@ def make_snap(device,snap_s):
 if __name__=="__main__":
     device,snap_s=get_cmd()
     make_snap(device,snap_s)
+    print(check_rootfs(snap_s))
